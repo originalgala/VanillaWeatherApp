3459419ebe6e8b27f50fb4d4c6414ecc4e530258
originalgala <133504883+originalgala@users.noreply.github.com>
2023-10-07 14:36:50 +0200

    Updated styling and changes API

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 function showTemperature(response) {
-    document.querySelector(".temperature").innerHTML = Math.round(response.data.main.temp); 
+    console.log(response.data)
+    document.querySelector(".temperature").innerHTML = Math.round(response.data.temperature.current); 
 
-    document.querySelector("#conditions").innerHTML = response.data.weather[0].main;
+    document.querySelector("#conditions").innerHTML = response.data.condition.description;
 
-    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#humidity").innerHTML = response.data.temperature.humidity;
 
     document.querySelector("#windspeed").innerHTML = Math.round(response.data.wind.speed);
 
-    document.querySelector("#city-display").innerHTML = response.data.name;
+    document.querySelector("#city-display").innerHTML = response.data.city;
+
+    document.querySelector("img") = response.data.icon_url;
 }
 
 
-let apiKey = "9e0fb79c2f66d0cd0dcf06710976a873"
-let url = `https://api.openweathermap.org/data/2.5/weather?q=rosslyn&appid=${apiKey}&units=metric`
+let apiKey = "00b38325ed040e371254ctd7ac1o8f0a"
+let url = `https://api.shecodes.io/weather/v1/current?query=pretoria&key=${apiKey}&units=metric`
 axios.get(url).then(showTemperature);
